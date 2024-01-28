93e6f6ee199f6e7fff63d1163c6d4ab17174f26c
SoniAryanXmart <154493242+Githubsoniaryan@users.noreply.github.com>
2024-01-28 19:38:55 +0530

    Update ping.py

--- VIPMUSIC/plugins/tools/ping.py
+++ VIPMUSIC/plugins/tools/ping.py
@@ -71,10 +71,10 @@ async def ping_com(client, message: Message, _):
         ],
         [
             InlineKeyboardButton(
-                text="✦ ɢʀᴏᴜᴘ ✦", url=f"https://t.me/TG_FRIENDSS",
+                text="✦ ɢʀᴏᴜᴘ ✦", url=f"https://t.me/royalgroupop",
             ),
             InlineKeyboardButton(
-                text="✧ ᴍᴏʀᴇ ✧", url=f"https://t.me/VIP_CREATORS",
+                text="✧ ᴍᴏʀᴇ ✧", url=f"https://t.me/itsRareBeautySenelaGomez",
             )
         ],
         [
