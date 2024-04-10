faca1b2a8958d9af3b1f970db96cdc4543ba9636
SoniAryanXmart <154493242+Githubsoniaryan@users.noreply.github.com>
2024-04-10 15:07:03 +0530

    Update sudoers.py

--- VIPMUSIC/plugins/sudo/sudoers.py
+++ VIPMUSIC/plugins/sudo/sudoers.py
@@ -59,7 +59,7 @@ async def sudoers_list(client, message: Message):
 async def check_sudo_list(client, callback_query: CallbackQuery):
     keyboard = []
     if callback_query.from_user.id not in SUDOERS:
-        return await callback_query.answer("𝗣𝗔𝗛𝗟𝗘 𝗔𝗡𝗜𝗠𝗔𝗟 𝗞𝗢 𝗝𝗔𝗞𝗘 𝗣𝗔𝗣𝗔 𝗕𝗢𝗟 🖕😎😂", show_alert=True)
+        return await callback_query.answer("𝗣𝗔𝗛𝗟𝗘 𓆩𝗔𝗡𝗜𝗠𝗔𝗟𓆪 𝗞𝗢 𝗝𝗔𝗞𝗘 𝗣𝗔𝗣𝗔 𝗕𝗢𝗟 🖕😎😂", show_alert=True)
     else:
         user = await app.get_users(OWNER_ID)
 
