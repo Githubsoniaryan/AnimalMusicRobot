945a6e0028557386b861efb8de6b4612b6625062
SoniAryanXmart <154493242+Githubsoniaryan@users.noreply.github.com>
2024-04-12 11:56:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -120,14 +120,14 @@ PING_IMG_URL = getenv(
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
+TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
+TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
+STREAM_IMG_URL = "https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
+SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
+YOUTUBE_IMG_URL = "https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
 
 
 def time_to_seconds(time):
